4e55c5fa2075f2dbda697ab3a6221df2b258cc40
anaika-pandey1710 <70497768+anaika-pandey1710@users.noreply.github.com>
2020-09-30 20:36:18 -0500

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,5 @@
+class Dustbin{
+    box1=createSprite(600,650,200,20);
+	box2=createSprite(500,610,20,100);
+	box3=createSprite(700,610,20,100);  
+    }
---
+++ paper.js
@@ -0,0 +1,11 @@
+class Paper{
+    constructor(x,y,width,height){
+    var options={
+        isstatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+    ellipse(56, 46, 55, 55);
+}
+}
